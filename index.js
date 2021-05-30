aaea200fd89b68df5b45d4f40cefecce835771ef
reflective-foobar-0518 <jamesspaeth1994@gmail.com>
2021-05-30 21:29:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,21 +17,17 @@ function subtract (a, b) {
 
 
 function inc(n) {
-  n++;
-  return n;
+  return n += 1;
 }
 
 function dec(n) {
-  --n;
-  return n;
+  return n -= 1;
 }
 
 function makeInt(n) {
-  parseInt(n, 10);
-  return n;
+  return parseInt(n, 10);
 }
 
 function preserveDecimal(n) {
-  parseFloat(n, 10);
-  return n;
+  return parseFloat(n, 10);
 }
